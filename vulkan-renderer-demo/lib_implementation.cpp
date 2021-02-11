e7b5343d9b1f867c1f2a6b955a1ce4d0428ec036
Anton Shalgachev <anton.shalgachev@gmail.com>
2021-02-11 21:44:56 +0100

    add tiny_gltf library

--- vulkan-renderer-demo/lib_implementation.cpp
+++ vulkan-renderer-demo/lib_implementation.cpp
@@ -1,8 +1,18 @@
 // stb_image and tiny_obj_loader produce this warning
 #pragma warning(disable:5045)
+#pragma warning(disable:4996)
 
 #define STB_IMAGE_IMPLEMENTATION
 #include <stb_image.h>
 
 #define TINYOBJLOADER_IMPLEMENTATION
 #include <tiny_obj_loader.h>
+
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include <stb_image_write.h>
+
+#define TINYGLTF_NO_INCLUDE_STB_IMAGE
+#define TINYGLTF_NO_INCLUDE_STB_IMAGE_WRITE
+#define TINYGLTF_IMPLEMENTATION
+#define TINYGLTF_USE_CPP14
+#include <tiny_gltf.h>
