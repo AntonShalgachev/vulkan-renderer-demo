dc18c9b82143d90059b56c7066c26497b22f6d67
Anton Shalgachev <anton.shalgachev@gmail.com>
2021-05-21 00:35:40 +0200

    fix shader binary path

--- vulkan-renderer-demo/data/shaders/compile_shaders.bat
+++ vulkan-renderer-demo/data/shaders/compile_shaders.bat
@@ -1,7 +1,7 @@
 cd shaders/
-C:\VulkanSDK\1.2.154.1\Bin32\glslc.exe default.vert -o bin/default.vert.spv -g -O0 -DHAS_NORMAL
-C:\VulkanSDK\1.2.154.1\Bin32\glslc.exe default.frag -o bin/default.frag.spv -g -O0 -DHAS_NORMAL
-C:\VulkanSDK\1.2.154.1\Bin32\glslc.exe normaldebug.geom -o bin/normaldebug.geom.spv -g -O0
-C:\VulkanSDK\1.2.154.1\Bin32\glslc.exe normaldebug.vert -o bin/normaldebug.vert.spv -g -O0
+C:\VulkanSDK\1.2.154.1\Bin32\glslc.exe default.vert -o compiled/default.vert.spv -g -O0 -DHAS_NORMAL
+C:\VulkanSDK\1.2.154.1\Bin32\glslc.exe default.frag -o compiled/default.frag.spv -g -O0 -DHAS_NORMAL
+C:\VulkanSDK\1.2.154.1\Bin32\glslc.exe normaldebug.geom -o compiled/normaldebug.geom.spv -g -O0
+C:\VulkanSDK\1.2.154.1\Bin32\glslc.exe normaldebug.vert -o compiled/normaldebug.vert.spv -g -O0
 cd ..
 rem pause
