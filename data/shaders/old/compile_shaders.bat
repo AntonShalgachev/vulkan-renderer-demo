0dda6d5d7453785bd07b5156b969931b9e74f406
Anton Shalgachev <anton.shalgachev@gmail.com>
2022-10-07 18:15:16 +0200

    remove old shaders

--- data/shaders/old/compile_shaders.bat
+++
@@ -1,7 +0,0 @@
-@REM cd shaders/
-T:\android\code\libs\VulkanSDK\Bin\glslc.exe default.vert -o compiled/default.vert.spv -g -O0 -DHAS_NORMAL
-T:\android\code\libs\VulkanSDK\Bin\glslc.exe default.frag -o compiled/default.frag.spv -g -O0 -DHAS_NORMAL
-T:\android\code\libs\VulkanSDK\Bin\glslc.exe normaldebug.geom -o compiled/normaldebug.geom.spv -g -O0
-T:\android\code\libs\VulkanSDK\Bin\glslc.exe normaldebug.vert -o compiled/normaldebug.vert.spv -g -O0
-@REM cd ..
-rem pause
