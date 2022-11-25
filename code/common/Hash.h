d502bb013df727b64bb18e3673a57f5896635b90
Anton Shalgachev <anton.shalgachev@gmail.com>
2022-11-26 00:17:12 +0100

    use nstl hash instead of std

--- code/common/Hash.h
+++ code/common/Hash.h
@@ -1,16 +1,17 @@
 #pragma once
 
-#include <cstddef>
-#include <functional>
+#include "nstl/hash.h"
+
+#include <stddef.h>
 
 namespace vkc
 {
 	namespace hash
 	{
 		template<typename T>
-		void combine(std::size_t& seed, T const& v)
+		void combine(size_t& seed, T const& v)
 		{
-			std::hash<T> hasher;
+			nstl::hash<T> hasher;
 			seed ^= hasher(v) + 0x9e3779b9 + (seed<<6) + (seed>>2);
 		}
 	}
