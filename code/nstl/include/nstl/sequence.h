a7ede6a956d1928db4828ef88494d42bd04cdf04
Anton Shalgachev <anton.shalgachev@gmail.com>
2023-03-27 00:27:43 +0200

    fix case in nstl::sequence

--- code/nstl/include/nstl/sequence.h
+++ code/nstl/include/nstl/sequence.h
@@ -4,22 +4,20 @@
 
 namespace nstl
 {
-    template<typename Type, Type... Is>
-    struct IntegerSequence
-    {
-    };
+    template<typename T, T... Is>
+    struct integer_sequence {};
 
     template<size_t... Is>
-    using IndexSequence = IntegerSequence<size_t, Is...>;
+    using index_sequence = integer_sequence<size_t, Is...>;
 
 #if defined(__clang__) || defined(_MSC_VER)
     template<size_t size>
-    using MakeIndexSequence = __make_integer_seq<IntegerSequence, size_t, size>;
+    using make_index_sequence = __make_integer_seq<integer_sequence, size_t, size>;
 #elif defined(__GNUC__) && __has_builtin(__make_integer_seq)
     template<size_t size>
-    using MakeIndexSequence = __make_integer_seq<IntegerSequence, size_t, size>;
+    using make_index_sequence = __make_integer_seq<integer_sequence, size_t, size>;
 #elif defined(__GNUC__)
     template<size_t size>
-    using MakeIndexSequence = IntegerSequence<size_t, __integer_pack(size)...>;
+    using make_index_sequence = integer_sequence<size_t, __integer_pack(size)...>;
 #endif
 }
