4af6005b99ce5eb8b7a38daa364675cf0f9dc2a2
Anton Shalgachev <anton.shalgachev@gmail.com>
2023-04-27 00:55:04 +0200

    add tglm console serializer

--- code/demo/console/GlmSerializer.cpp
+++ code/demo/console/GlmSerializer.cpp
@@ -6,6 +6,8 @@
 
 #include "glm.h"
 
+#include "tglm/types.h"
+
 coil::Expected<glm::vec3, coil::String> coil::TypeSerializer<glm::vec3>::fromString(Value const& input)
 {
     if (input.subvalues.size() != N)
@@ -41,3 +43,39 @@ coil::String coil::TypeSerializer<glm::vec3>::toString(glm::vec3 const& value)
 }
 
 COIL_CREATE_TYPE_NAME_DEFINITION(glm::vec3, "vec3");
+
+coil::Expected<tglm::vec3, coil::String> coil::TypeSerializer<tglm::vec3>::fromString(Value const& input)
+{
+    if (input.subvalues.size() != N)
+        return errors::createMismatchedSubvaluesError<tglm::vec3>(input, N);
+
+    tglm::vec3 result;
+    for (size_t i = 0; i < N; i++)
+    {
+        auto maybeValue = TypeSerializer<ElementType>::fromString(input.subvalues[i]);
+        if (!maybeValue)
+            return errors::createGenericError<tglm::vec3>(input, maybeValue.error());
+
+        result[i] = *nstl::move(maybeValue);
+    }
+
+    return result;
+}
+
+coil::String coil::TypeSerializer<tglm::vec3>::toString(tglm::vec3 const& value)
+{
+    coil::String result = "(";
+    coil::StringView separator = "";
+
+    for (size_t i = 0; i < N; i++)
+    {
+        result += separator;
+        result += coil::toString(value[i]);
+        separator = ", ";
+    }
+
+    result += ")";
+    return result;
+}
+
+COIL_CREATE_TYPE_NAME_DEFINITION(tglm::vec3, "vec3");
--- code/demo/console/GlmSerializer.h
+++ code/demo/console/GlmSerializer.h
@@ -3,6 +3,8 @@
 #include "coil/Coil.h" // TODO forward-declare?
 #include "glm-fwd.h"
 
+#include "tglm/fwd.h"
+
 namespace coil
 {
     // TODO cover other glm types
@@ -18,4 +20,18 @@ namespace coil
     };
 
     COIL_CREATE_TYPE_NAME_DECLARATION(glm::vec3);
+
+    // TODO cover other glm types
+    template<>
+    struct TypeSerializer<tglm::vec3>
+    {
+        static size_t const N = 3;
+        using ElementType = float;
+
+        static Expected<tglm::vec3, coil::String> fromString(Value const& input);
+
+        static coil::String toString(tglm::vec3 const& value);
+    };
+
+    COIL_CREATE_TYPE_NAME_DECLARATION(tglm::vec3);
 }
