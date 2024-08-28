603dbb211d7f58d5d842eaa187d5d1832e1434c0
Anton Shalgachev <anton.shalgachev@gmail.com>
2024-08-28 22:22:00 +0200

    logging: output whole log entry instead of outputting it by parts

--- code/logging/src/logging.cpp
+++ code/logging/src/logging.cpp
@@ -7,38 +7,14 @@
 
 #include "stdio.h"
 
-namespace
-{
-    struct log_writer : public picofmt::writer
-    {
-        bool write(nstl::string_view str) override
-        {
-            if (str.empty())
-                return true;
-
-            platform::debug_output(str);
-
-            return true;
-        }
-
-        void report_error(nstl::string_view) override
-        {
-            // TODO replace with something that would crash in Release
-            // TODO make use of the error string
-            assert(false);
-        }
-    };
-}
-
 namespace logging
 {
     void log(level level, nstl::string_view str, nstl::source_location loc)
     {
         MEMORY_TRACKING_SCOPE(get_scope_id());
 
-        log_writer out{};
-        [[maybe_unused]] bool res = picofmt::format_to(out, "{}({},{}): [{:!}] {}\n", loc.file_name(), loc.line(), loc.column(), level, str);
-        assert(res);
+        nstl::string result = common::format("{}({},{}): [{:!}] {}\n", loc.file_name(), loc.line(), loc.column(), level, str);
+        platform::debug_output(result);
     }
 
     void vlogf(level level, nstl::string_view format, picofmt::args_list const& args, nstl::source_location loc)
